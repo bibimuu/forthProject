ad75f8d3adb741a5ea59b6acfe2791e4427b238e
平松大悟朗 <h.dai56@gmail.com>
2020-06-03 21:14:40 +0900

    section15 277 navigation5+

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import * as Font from "expo-font";
 import ReduxThunk from "redux-thunk";
 
 import productsReducer from "./store/reducers/products";
-import NavigationContainer from "./navigation/NavigationContainer";
+import AppNavigator from "./navigation/AppNavigator";
 import cartReducer from "./store/reducers/cart";
 import ordersReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
@@ -43,7 +43,7 @@ export default function App() {
   }
   return (
     <Provider store={store}>
-      <NavigationContainer/>
+      <AppNavigator/>
     </Provider>
   );
 }
